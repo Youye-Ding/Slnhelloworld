32e741f3c7f7cb1b296b451eb52d025c9b7a7a26
又也 丁 <dyy1858@126.com>
2020-03-28 12:35:55 +0800

    up

--- Slnhelloworld/calLoan2.aspx.cs
+++ Slnhelloworld/calLoan2.aspx.cs
@@ -11,6 +11,7 @@ namespace Slnhelloworld
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            //等额本金
             double loan = 1000000;
             int deadLine = 36;
             double rate = 0.049/12;
@@ -18,6 +19,15 @@ namespace Slnhelloworld
             for (int month = 0; month <= deadLine - 1; month++)
                 interest += (loan - month * loan / deadLine) * rate;
             Response.Write(String.Format("共计支付本息{0}元，其中利息{1}元。", loan + interest, interest));
+
+            //等额本息
+            double loan1 = loan * Math.Pow(1 + rate, deadLine);
+            double krate = 0;
+            for (int i = 0; i < deadLine; i++)
+                krate += Math.Pow(1 + rate, i);
+            double pay = loan1 / krate;
+            Response.Write(String.Format("每月支付金额{0}元，共计支付本息{1}元，其中利息{2}元。", pay, pay*deadLine,pay*deadLine - loan));
+
         }
     }
 }
