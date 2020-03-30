5cd8ddf3505d79707cf478d2a0c0919b325113c0
又也 丁 <dyy1858@126.com>
2020-03-30 21:31:40 +0800

    u

---
+++ ProjectionAlgorithm/ExampleMonteCarloPi.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ExampleMonteCarloPi.aspx.cs" Inherits="ProjectionAlgorithm.ExampleMonteCarloPi" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:TextBox ID="txtNum" runat="server" OnTextChanged="txtNum_TextChanged"></asp:TextBox>
+        <p>
+            <asp:CheckBox ID="chkPrintPoint" runat="server" OnCheckedChanged="chkPrintPoint_CheckedChanged" Text="显示随机样本" />
+        </p>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="求Pi" />
+    </form>
+</body>
+</html>
---
+++ ProjectionAlgorithm/ExampleMonteCarloPi.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProjectionAlgorithm
+{
+    public partial class ExampleMonteCarloPi : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        private int nums = 100000;
+        protected void txtNum_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void chkPrintPoint_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            double x, y;
+            double p;
+            double m;
+            double pi;
+            nums = int.Parse(txtNum.Text);
+            Random rand = new Random();
+            m = 0;
+            for (int i=0;i<nums;i++)
+            {
+                x = rand.NextDouble() * 2 - 1;
+                y = rand.NextDouble() * 2 - 1;
+                if (x*x+y*y<=1)
+                {
+                    m++;
+                }
+                if (chkPrintPoint.Checked)
+                    Response.Write(string.Format("</br>x={0},y={1}", x, y));
+            }
+            p = m / nums;
+            pi = 4 * p;
+            Response.Write("</br>落在圆区域内的次数：" + m);
+            Response.Write("</br>随机点落在圆区域的概率：" + p);
+            Response.Write("</br>π的值为：" + pi);
+        }
+    }
+}
