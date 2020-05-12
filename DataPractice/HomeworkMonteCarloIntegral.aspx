6b27f349eaa2be1cffecc95ed76e6f1e225e0994
又也 丁 <dyy1858@126.com>
2020-05-12 17:21:42 +0800

    up

---
+++ DataPractice/HomeworkMonteCarloIntegral.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="HomeworkMonteCarloIntegral.aspx.cs" Inherits="DataPractice.HomwworkMonteCarloIntegral" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        请输入随机样本的数量：<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="单击计算e值" />
+    </form>
+</body>
+</html>
---
+++ DataPractice/HomeworkMonteCarloIntegral.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DataPractice
+{
+    public partial class HomwworkMonteCarloIntegral : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int nums = int.Parse(TextBox1.Text);
+            double x, y;
+            double p = 0;//概率
+            double m = 0;//随机点落入规定面积中的数量
+            double s = 0.0;//积分值
+            double exp = 0.0;
+            Random rand = new Random();
+            for (int i = 0; i <= nums; i++)
+            {
+                x = rand.NextDouble() * (-1) + 2;
+                y = rand.NextDouble() * (-1) + 1;
+                if (y <= 1 / x)
+                {
+                    m++;
+                }
+            }
+            p = m / nums;
+            s = p;
+            exp = Math.Pow(2, 1 / s);
+            Response.Write("e的估计值为" + exp);
+        }
+    }
+}
