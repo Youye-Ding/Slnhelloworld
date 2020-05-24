a559e023df30c6cd494d140d74d8b3db7a4d6cb4
又也 丁 <dyy1858@126.com>
2020-05-24 18:22:57 +0800

    up

---
+++ DataPractice/HomeworkBayesIris.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="HomeworkBayesIris.aspx.cs" Inherits="DataPractice.HomeworkBayesIris" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            花萼长度：<asp:TextBox ID="CalyxLength" runat="server" OnTextChanged="CalyxLength_TextChanged"></asp:TextBox>
+            <br />
+            <br />
+            花萼宽度：<asp:TextBox ID="CalyxWidth" runat="server" OnTextChanged="CalyxWidth_TextChanged"></asp:TextBox>
+            <br />
+            <br />
+            花瓣长度：<asp:TextBox ID="PetalLength" runat="server" OnTextChanged="PetalLength_TextChanged"></asp:TextBox>
+            <br />
+            <br />
+            花萼宽度：<asp:TextBox ID="PetalWidth" runat="server" OnTextChanged="PetalWidth_TextChanged"></asp:TextBox>
+        </div>
+        <p>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="单击预测鸢尾兰类型" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ DataPractice/HomeworkBayesIris.aspx.cs
@@ -0,0 +1,75 @@
+﻿using SQL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Common.AITools.Tvbboy;
+
+namespace DataPractice
+{
+    public partial class HomeworkBayesIris : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            double calyxLength = double.Parse(CalyxLength.Text);
+            double calyxWidth = double.Parse(CalyxWidth.Text);
+            double petalLength = double.Parse(PetalLength.Text);
+            double petalWidth = double.Parse(PetalWidth.Text);
+            string sql = "select Label, CalyxLength, CalyxWidth, PetalLength, PetalWidth from tblIris";
+            SQLHelper sh = new SQLHelper();
+            DataTable dt = new DataTable();
+            DataSet ds = new DataSet();
+            try
+            {
+                sh.RunSQL(sql, ref ds);
+                if (ds.Tables[0] != null)
+                {
+                    dt = ds.Tables[0];
+                    if (dt.Rows.Count > 0)
+                    {
+                        ClassNaiveBayes classifier = new ClassNaiveBayes();
+                        classifier.TrainClassifier(dt);
+                        Response.Write(classifier.Classify(new double[] { calyxLength, calyxWidth, petalLength, petalWidth }));
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+            finally
+            {
+                sh.Close();
+            }
+            
+        }
+
+        protected void CalyxLength_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void CalyxWidth_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void PetalLength_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void PetalWidth_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
