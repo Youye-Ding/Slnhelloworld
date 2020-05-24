7dd3d87eb688f766c85e2ff0c326781779105216
又也 丁 <dyy1858@126.com>
2020-05-24 19:11:00 +0800

    up

---
+++ DataPractice/HomeworkBayesGender.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="HomeworkBayesGender.aspx.cs" Inherits="DataPractice.HomeworkBayesGender" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            脚长：<asp:TextBox ID="FootSize" runat="server" OnTextChanged="FootSize_TextChanged"></asp:TextBox>
+            <br />
+            <br />
+            体重：<asp:TextBox ID="Weight" runat="server" OnTextChanged="Weight_TextChanged"></asp:TextBox>
+            <br />
+            <br />
+            身高：<asp:TextBox ID="Height" runat="server" OnTextChanged="Height_TextChanged"></asp:TextBox>
+        </div>
+        <p>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="单击按钮推断性别" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ DataPractice/HomeworkBayesGender.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using SQL;
+using Common.AITools.Tvbboy;
+
+namespace DataPractice
+{
+    public partial class HomeworkBayesGender : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            double footSize = double.Parse(FootSize.Text);
+            double height = double.Parse(Height.Text);
+            double weight = double.Parse(Weight.Text);
+            string sql = "select (case gender when 1 then '男性' else '女性' end) as Gender, Footsize, Weight, Height from tblBodies";
+            SQLHelper sh = new SQLHelper();
+            DataTable dt = new DataTable();
+            DataSet ds = new DataSet();
+            try
+            {
+                sh.RunSQL(sql, ref ds);
+                if (ds.Tables[0] != null)
+                {
+                    dt = ds.Tables[0];
+                    if (dt.Rows.Count > 0)
+                    {
+                        ClassNaiveBayes classifier = new ClassNaiveBayes();
+                        classifier.TrainClassifier(dt);
+                        Response.Write(classifier.Classify(new double[] { footSize, height, weight }));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+            finally
+            {
+                sh.Close();
+            }
+        }
+
+        protected void FootSize_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Weight_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Height_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
