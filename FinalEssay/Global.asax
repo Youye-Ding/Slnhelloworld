d01520c2e4330de51f8e810ed337c109fa83e6c3
又也 丁 <dyy1858@126.com>
2020-06-06 17:14:59 +0800

    up

---
+++ FinalEssay/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FinalEssay.Global" Language="C#" %>
--- ProjectCrawler/ExampleCrawLink.aspx.cs
+++ ProjectCrawler/ExampleCrawLink.aspx.cs
@@ -20,39 +20,39 @@ namespace ProjectCrawler
         public void HrefCrawler()
         {
             var hrefList = new List<examplemyhref>();
-            string initurl = "https://tieba.baidu.com/f?kw=%E5%8D%8E%E4%B8%9C%E5%B8%88%E8%8C%83%E5%A4%A7%E5%AD%A6&ie=utf-8";
-            string result = string.Empty;
-            var hrefCrawler = new SimpleCrawler();
-            hrefCrawler.url = new Uri(initurl);
-            Response.Write("爬虫开始抓取地址：" + hrefCrawler.url.ToString() + "</br>");
-            hrefCrawler.OnError += (s, e) =>
-             {
-                 Response.Write("爬虫抓取出现错误，异常信息：" + e.Exception.Message);
-             };
-            hrefCrawler.OnCompleted += (s, e) =>
-              {
-                  var links = Regex.Matches(e.PageSource, @"<a[^>]+href=""*(?<href>[^>\s]+)""\s*[^>]*>(?<text>(?!.*img).*?)</a>", RegexOptions.IgnoreCase);
-                  foreach(Match match in links)
+            for (int page = 0; page < 10; page++)
+            {
+                string initurl = string.Format("https://tieba.baidu.com/f?kw=%E5%8D%8E%E4%B8%9C%E5%B8%88%E8%8C%83%E5%A4%A7%E5%AD%A6&ie=utf-8&&pn={0}", page*50);
+                string result = string.Empty;
+                var hrefCrawler = new SimpleCrawler();
+                hrefCrawler.url = new Uri(initurl);
+                hrefCrawler.OnError += (s, e) =>
+                 {
+                     Response.Write("爬虫抓取出现错误，异常信息：" + e.Exception.Message);
+                 };
+                hrefCrawler.OnCompleted += (s, e) =>
                   {
-                      var h = new examplemyhref
+                      var links = Regex.Matches(e.PageSource, @"<a[^>]+href=""*(?<href>[^>\s]+)""\s*[^>]*>(?<text>(?!.*img).*?)</a>", RegexOptions.IgnoreCase);
+                      foreach (Match match in links)
                       {
-                          hreftitle = match.Groups["text"].Value,
-                          hrefsrc = match.Groups["href"].Value
-                      };
-                      if (!hrefList.Contains(h))
-                      {
-                          hrefList.Add(h);
-                          result += h.hreftitle + "|" + h.hrefsrc + "</br>";
+                          var h = new examplemyhref
+                          {
+                              hreftitle = match.Groups["text"].Value,
+                              hrefsrc = match.Groups["href"].Value
+                          };
+                          if (!hrefList.Contains(h)&&(h.hreftitle.Contains("求助")||h.hreftitle.Contains("考研")||h.hreftitle.Contains("学长")||h.hreftitle.Contains("学姐")))
+                          {
+                              hrefList.Add(h);
+                              result += h.hreftitle + "|" + @"https://tieba.baidu.com" + h.hrefsrc + "</br>";
+                          }
                       }
-                  }
-                  Response.Write("================================</br>");
-                  Response.Write("爬虫抓取任务完成！合计" + links.Count + "个超级链接。</br>");
-                  Response.Write("耗时：" + e.Milliseconds + "</br>毫秒");
-                  Response.Write("线程：" + e.ThreadId + "</br>");
-                  Response.Write(result);
-                  Response.Write("================================</br>");
-              };
-            hrefCrawler.start();
+                      Response.Write("================================</br>");
+                      Response.Write(string.Format("第{0}页</br>", page + 1));
+                      Response.Write(result);
+                      Response.Write("================================</br>");
+                  };
+                hrefCrawler.start();
+            }
         }
 
         protected void Page_Load(object sender, EventArgs e)
